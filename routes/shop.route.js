fe2b9972177a5452908f2aa3dae4650df86e0a1f
FanantenanaKantoMaminiaina <fanantenanakanto27@gmail.com>
2026-01-29 10:31:05 +0300

    [ADD] : Routes security

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -1,12 +1,14 @@
 const shopController = require('../controllers/shop.controller');
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authentication.middleware');
+const { ROLE } = require('../data/Role');
 
-router.post("/", shopController.save);
 router.get("/", shopController.getAll);
-router.put("/:id", shopController.update);
-router.delete("/:id", shopController.remove);
-router.patch("/:id/activate", shopController.activate);
-router.patch("/:id/deactivate", shopController.deactivate);
+router.post("/", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.save);
+router.put("/:id", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.update);
+router.delete("/:id", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.remove);
+router.patch("/:id/activate", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.activate);
+router.patch("/:id/deactivate", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.deactivate);
 
 module.exports = router;
