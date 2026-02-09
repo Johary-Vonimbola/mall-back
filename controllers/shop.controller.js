197b409d8041106b47362a9e2253af5156d2c81b
FanantenanaKantoMaminiaina <fanantenanakanto27@gmail.com>
2026-02-09 17:05:14 +0300

    [ADD] : upload logo for shop and modif shop

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -1,5 +1,6 @@
 const Shop = require('../models/Shop');
 const ApiResponse = require('../utils/ApiResponse');
+const { PathLogoShop } = require('../data/PathUpload');
 
 const getAll = async (req, res) => {
     try {
@@ -8,6 +9,42 @@ const getAll = async (req, res) => {
             200,
             "Shop record(s)",
             shops
+        ));
+    } catch (err) {
+        return res.status(500).json(ApiResponse.error(
+            500,
+            "Error retrieving shops",
+            [err.message]
+        ));
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json(ApiResponse.error(
+                400,
+                'Error fecthing shop',
+                ['No id provided']
+            ));
+        }
+
+        const shop = await Shop.findById(id);
+
+        if (!shop) {
+            return res.status(404).json(ApiResponse.error(
+                404,
+                'Shop not found',
+                ['Shop does not exist']
+            ));
+        }
+
+        return res.status(200).json(ApiResponse.succes(
+            200,
+            `Shop ${id} record`,
+            shop
         ));
     } catch (err) {
         return res.status(500).json(ApiResponse.error(
@@ -41,6 +78,54 @@ const save = async (req, res) => {
         return res.status(500).json(ApiResponse.error(
             500,
             'Error creating shop',
+            [err.message]
+        ));
+    }
+};
+
+
+const upload = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json(ApiResponse.error(
+                400,
+                'Error updating shop',
+                ['No id provided']
+            ));
+        }
+
+        const updateData = { ...req.body };
+
+        if (req.file) {
+            updateData.logo = `${PathLogoShop}/${req.file.filename}`;
+        }
+
+        const shop = await Shop.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true }
+        );
+
+        if (!shop) {
+            return res.status(404).json(ApiResponse.error(
+                404,
+                'Shop not found',
+                ['Shop does not exist']
+            ));
+        }
+
+        return res.status(200).json(ApiResponse.succes(
+            200,
+            'Shop updated',
+            shop
+        ));
+
+    } catch (err) {
+        return res.status(500).json(ApiResponse.error(
+            500,
+            'Error updating shop',
             [err.message]
         ));
     }
@@ -216,7 +301,9 @@ const deactivate = async (req, res) => {
 
 module.exports.save = save;
 module.exports.getAll = getAll;
+module.exports.getById = getById;
 module.exports.update = update;
+module.exports.upload = upload;
 module.exports.remove = remove;
 module.exports.activate = activate;
 module.exports.deactivate = deactivate;
--- routes/shop.route.js
+++ routes/shop.route.js
@@ -2,11 +2,24 @@ const shopController = require('../controllers/shop.controller');
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authentication.middleware');
+const upload = require("../middlewares/upload.middleware");
 const { ROLE } = require('../data/Role');
+const { PathLogoShop } = require('../data/PathUpload');
 
 router.get("/", shopController.getAll);
+router.get("/:id", shopController.getById);
 router.post("/", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.save);
-router.put("/:id", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.update);
+
+router.post('/:id/upload', authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.SHOP), 
+  (req, res, next) => {
+    req.uploadPath = PathLogoShop;
+    next();
+  },
+  upload.single('logo'),
+  shopController.upload
+);
+
+router.put("/:id", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.SHOP), shopController.update);
 router.delete("/:id", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.remove);
 router.patch("/:id/activate", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.activate);
 router.patch("/:id/deactivate", authMiddleware.authenticateToken, authMiddleware.authenticateRole(ROLE.ADMIN), shopController.deactivate);
