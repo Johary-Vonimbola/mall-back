01e0157f8dec653c40c976b4057436dc71d393a5
Johary-Vonimbola <joharyvonimbola@gmail.com>
2026-02-20 09:17:55 +0300

    [ADD]: shop rent payment filters admin

--- routes/shopRentPayment.route.js
+++ routes/shopRentPayment.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getRentsByYear, createRentPayment, update } = require('../controllers/shopRentPayment.controller');
+const { getRentsByYear, createRentPayment, update, getAll } = require('../controllers/shopRentPayment.controller');
 const { authenticateToken, authenticateRole } = require('../middlewares/authentication.middleware');
 const { ROLE } = require('../data/Role');
 
 const router = express.Router();
 
+router.get('', authenticateToken, authenticateRole(ROLE.ADMIN), getAll);
 router.get('/:shopId/:year', authenticateToken, authenticateRole(ROLE.SHOP), getRentsByYear);
 router.post("", authenticateToken, authenticateRole(ROLE.SHOP), createRentPayment);
 router.put("/:rentId", authenticateToken, authenticateRole(ROLE.SHOP), update);
