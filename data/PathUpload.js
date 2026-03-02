8632b753882f585abaebb6105e363e80810576d4
FanantenanaKantoMaminiaina <fanantenanakanto27@gmail.com>
2026-03-02 14:49:26 +0300

    [FIX] : modify upload to cloudinary

--- data/PathUpload.js
+++ data/PathUpload.js
@@ -1,5 +1,9 @@
-const PathLogoShop = "uploads/shop/logo";
-const PathPictureProduct = "uploads/product/picture";
+// const PathLogoShop = "uploads/shop/logo";
+// const PathPictureProduct = "uploads/product/picture";
+
+const PathLogoShop = "shop";
+const PathPictureProduct = "product";
 
 module.exports.PathLogoShop = PathLogoShop;
 module.exports.PathPictureProduct = PathPictureProduct;
+ 
