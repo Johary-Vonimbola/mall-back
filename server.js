b7253c7c771e66a4d409bbc67ca61d7c0b557097
Johary-Vonimbola <joharyvonimbola@gmail.com>
2026-03-03 07:34:02 +0300

    [ADD]: env connexion

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 async function startServer() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            serverSelectionTimeoutMS: 10000
+            serverSelectionTimeoutMS: process.env.CONNEXION_TIME
         });
 
         console.log('------------------MongoDB connected------------------');
